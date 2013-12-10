241eacaa54b3a4777ea08a196f0071381a5ea6de
W. Ryan Nestor <nestor@fuzzy-math.com>
2013-12-10 14:16:47 -0500

    non generic app logo

--- Resources/JS/functions_dev-ck.js
+++
@@ -1,1 +0,0 @@
-function air(){}function DBConnector(){}air.trace=function(e){console.log(e)};DBConnector.prototype.open=function(){air.trace("fake open")};DBConnector.prototype.queryByIds=function(e){air.trace("fake query");$.event.trigger({type:"FRED",observations:sample_data[e.g],title:"Series Title Goes Here"})};
