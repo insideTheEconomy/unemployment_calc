f522588ec3959ea7c0bb61f23658f0e98470e01c
Ray Ortgiesen <ray.ortgiesen@gmail.com>
2014-02-12 14:15:15 -0500

    Changes w/ Ryan

--- Resources/JS/db_functions.js
+++ Resources/JS/db_functions.js
@@ -1,11 +1,14 @@
 	//console.log("Functions Loaded")
-	 
+	
+	var when = require('when')
 	var sqlite3 = require('sqlite3').verbose();
 	var fs = require("fs");
+	
 	//var db = new sqlite3.Database('../unemployment.db');
 
 	function DBConnector () {
 		this.rows;
+		var self = this;
 	}
 	
 	dbp = DBConnector.prototype;
@@ -32,7 +35,7 @@
 		rows = this.db.execute(q);
 		var obs = JSON.parse(rows.fieldByName("observations"));*/
 
-		obD = this.getData (ids);
+	/*	obD = this.getData (ids);
 		if(obD){
 			$.event.trigger({
 						type: "FRED",
@@ -44,11 +47,28 @@
 						type: "ERROR",
 						text: "no record found in database",
 					});
-		}
+		}*/
 		
 	}
 	
+	dbp.getBaseline = function(ids){
+	/*	var q = "SELECT * FROM ser_data WHERE ser_data.ser_id = (SELECT series_id FROM ser_id WHERE g_id LIKE "+ids.g+" AND ed_id  LIKE "+ids.ed+" AND ar_id LIKE "+ids.ar+")"
+		rows = this.db.execute(q);
+		var obs = JSON.parse(rows.fieldByName("observations"));*/
+
+		this.getData( { "g" : 0, "ed" : 0, "ar" : 0  }).then(function (d){
+			console.log("dfd-");
+			console.log(d);
+			$(self).event.trigger({
+						type: "BASELINE",
+						observations: d[0].obs,
+						series: d[0].ser.seriess[0]
+					});
+			});
+	}
+	
 	dbp.getData = function( ids ){
+		var dfd = when.defer();
 		console.log("this.db (getData)= " + this.db);
 		var q = "SELECT * FROM ser_data WHERE ser_data.ser_id = (SELECT series_id FROM ser_id WHERE g_id LIKE "+ids.g+" AND ed_id  LIKE "+ids.ed+" AND ar_id LIKE "+ids.ar+")"
 		this.rows = this.db.all(q, function (err, rows) {
@@ -58,12 +78,14 @@
 			
 			if (rows.length > 0) {
 				//cant figure out how to parse fields out of this row object
-				console.log("rows: " rows);
+				console.log("rows");
+				console.log(rows);
+				dfd.resolve(rows[0]);
 			}
 		});
 		
 		/*
-		if(this.rows != null){
+		if(this.rows > 0){
 			ret = {}
 			ret.obs = JSON.parse(this.rows.fieldByName("observations"));
 			ret.ser = JSON.parse(this.rows.fieldByName("series"));
@@ -79,14 +101,16 @@
 		}else{
 			return false;
 		}*/
-		
+		return dfd.promise;
 	}
 	
 	Object.defineProperty(dbp, "baseline",{
 		get: function(){
-			data = this.getData( { "g" : 0, "ed" : 0, "ar" : 0  });
-			ret = data.obs;
-			return ret },
+			
+			data = this.getData( { "g" : 0, "ed" : 0, "ar" : 0  }).then(function (d){console.log("dfd-"); console.log(d)});
+			//ret = data.obs;
+			//return ret },
+			},
 		set: function(ds){
 			this.data = ds;
 			//console.log("DATASET RECEIVED");
